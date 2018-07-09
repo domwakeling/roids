ce60b08236ce8db2165a2290b1cef519aebd013c
Dom Wakeling <domwakeling@gmail.com>
2018-07-09 19:21:23 +0100

    Add high score / spawn more roids when cleared

--- src/js/game.js
+++ src/js/game.js
@@ -10,14 +10,18 @@ import {
 } from "./utilities.js";
 
 // get canvas, score and context
-let canv = document.getElementById("gameCanvas");
-let ctx = canv.getContext("2d");
-let scoreBox = document.getElementById("score");
+const canv = document.getElementById("gameCanvas");
+const ctx = canv.getContext("2d");
+const scoreBox = document.getElementById("score");
+const highScoreBox = document.getElementById("highscore");
 
 // global globals
 const FPS = 30; // frames per second
-let running = true;
+let running = true; // true when game is live
 let score = 0;
+let highScore = 0 // TODO: change this to find from store
+const TIME_BETWEEN_ROUNDS = 3000; // time (miliseconds) before new roids spawn
+let respawnTriggered = false; // bool to check whether a respawn is underway
 
 // ship globals
 const SHIP_SIZE = 15; // sizes ship
@@ -36,19 +40,37 @@ const ROID_SCORES = [10, 25, 50]; // points for each size
 const ROID_SPEED = 40; // pixles per second
 const ROID_NUM = 5; // number of starting roids, first level
 const roids = [];
+let roidsThisRound = ROID_NUM; // number of roids to add at start of the current round
 
 // get a ship
 const ship = new Ship(SHIP_SIZE, canv.width / 2, canv.height / 2);
 
-// get some roids
-let num_roids = ROID_NUM;
-for (let i = 0; i < num_roids; i++) {
-    const newPos = getSafeRoidLocation(canv.width, canv.height, ship.pos, SAFE_ZONE);
-    const r = new Roid(
-        ROID_SIZES[0], 0, randomNumVertices(), newPos.x, newPos.y, randomHeading(), ROID_SPEED / FPS
-    );
-    roids.push(r);
+function resetGame() {
+    ship.pos.x = canv.width / 2;
+    ship.pos.y = canv.width / 2;
+    ship.bullets = [];
+    ship.heading = 90;
+    roids = [];
+    roidsThisRound = ROID_NUM;
+    score = 0;
+    running = true;
 }
+
+
+// function for getting some roids
+function getNewRoids() {
+    for (let i = 0; i < roidsThisRound; i++) {
+        const newPos = getSafeRoidLocation(canv.width, canv.height, ship.pos, SAFE_ZONE);
+        const r = new Roid(
+            ROID_SIZES[0], 0, randomNumVertices(), newPos.x, newPos.y, randomHeading(), ROID_SPEED / FPS
+        );
+        roids.push(r);
+    }
+    roidsThisRound += 1;
+}
+
+// get some roids
+getNewRoids();
 
 // add event handlers
 document.addEventListener("keydown", keyDownHelper);
@@ -66,6 +88,15 @@ function keyUpHelper(e) {
 function update() {
     // break if not running
     if (!running) return;
+
+    // start a respawn if appropriate
+    if (roids.length === 0 && !respawnTriggered) {
+        respawnTriggered = true;
+        setTimeout(() => {
+            respawnTriggered = false;
+            getNewRoids();
+        }, TIME_BETWEEN_ROUNDS);
+    }
 
     // draw blank canvas;
     ctx.fillStyle = "black";
@@ -102,6 +133,7 @@ function update() {
                         roids.push(newRoidB);
                     }
                     score += ROID_SCORES[roids[i].sizeClass];
+                    if (score > highScore) highScore = score;
                     roids.splice(i, 1);
                     ship.bullets.splice(j, 1);
                     break;
@@ -129,6 +161,7 @@ function update() {
 
     // updaate score on screen
     scoreBox.innerText = `Score: ${score}`;
+    highScoreBox.innerText = `High Score: ${highScore}`;
 
 }
 
